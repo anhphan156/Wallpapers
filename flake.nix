37018bbb70ab1add49ae46b68c3aacce92bcf823
backspace <anh.phan156@protonmail.com>
2025-02-26 04:03:12 -0500

    turned into an overlay

--- flake.nix
+++ flake.nix
@@ -5,20 +5,16 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {inherit system;};
-
-    wallpapers = pkgs.stdenv.mkDerivation {
-      pname = "wallpapers";
-      version = "1.0.0";
-      src = ./.;
-      installPhase = ''
-        mkdir -p $out
-        cp -r src/* $out
-      '';
+  outputs = _: {
+    overlays.default = _: prev: {
+      wallpapers = prev.stdenvNoCC.mkDerivation {
+        name = "Wallpapers";
+        src = ./.;
+        installPhase = ''
+          mkdir -p $out
+          cp -r ./src/* $out
+        '';
+      };
     };
-  in {
-    packages."${system}".default = wallpapers;
   };
 }
